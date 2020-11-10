ff946db65330bf71a5f332638b39147d55d354f8
Nahid2194 <nahidjc80@gmail.com>
2020-11-10 11:30:46 +0600

    fire base google connect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
-
+import firebase from "firebase/app";
+import "firebase/auth"
+import firebaseConfig from './firebase.config';
+import { useState } from 'react';
+firebase.initializeApp(firebaseConfig)
 function App() {
+  const [user, setUser] = useState({
+    isSignedIn: false,
+    name: '',
+    email: '',
+    photo: ''
+  })
+  const provider = new firebase.auth.GoogleAuthProvider();
+  const handleSignout = () => {
+    firebase.auth().signOut()
+      .then(res => {
+        const signOutUser = {
+          isSignedIn: false,
+          name: '',
+          email: '',
+          photo: ''
+        }
+        setUser(signOutUser);
+      })
+      .catch(err => {
+        // An error happened.
+      });
+  }
+  const handleSignIn = () => {
+    firebase.auth().signInWithPopup(provider)
+      .then(res => {
+        const { email, photoURL, displayName } = res.user;
+        console.log(email, photoURL, displayName);
+        const signedInUser = {
+          isSignedIn: true,
+          name: displayName,
+          email: email,
+          photo: photoURL
+        }
+        setUser(signedInUser);
+      })
+      .catch(err => {
+        console.log(err);
+        console.log(err.message);
+      })
+    console.log("Sign Clicked");
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        user.isSignedIn ? <button className='btn btn-primary' onClick={handleSignout}>Sign out</button> :
+          <button className='btn btn-primary' onClick={handleSignIn}>Sign in</button>
+      }
+      <div style={{ border: "1px slid red" }} className="container">
+        {
+          user.isSignedIn && <div>
+            <p>Welcome , {user.name}</p><br />
+            <p>Your Email : {user.email}</p><br />
+            <img src={user.photo} alt="" />
+          </div>
+
+
+        }
+      </div>
     </div>
   );
 }
---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB5yDaYMtDddJcF248bVvel9yCBJiWS46g",
+    authDomain: "fir-check-75307.firebaseapp.com",
+    databaseURL: "https://fir-check-75307.firebaseio.com",
+    projectId: "fir-check-75307",
+    storageBucket: "fir-check-75307.appspot.com",
+    messagingSenderId: "278509579304",
+    appId: "1:278509579304:web:20becd1a5cf945ee9c591d"
+};
+export default firebaseConfig;
